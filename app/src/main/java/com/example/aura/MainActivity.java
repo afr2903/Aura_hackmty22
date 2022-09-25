ce6acf1dcf48243d7cc864624f8975f72cbdac5a
Ivan02A <114321245+Ivan02A@users.noreply.github.com>
2022-09-25 00:19:42 -0500

    Cambios en la pagina de graficas

--- app/src/main/java/com/example/aura/MainActivity.java
+++ app/src/main/java/com/example/aura/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -32,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
         }
         );
 
+        Intent i = new Intent(this, Estadistica.class);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(i);
+
+            }
+        });
 
     }
 
