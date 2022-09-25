dee968cdbdd7c270f4d18ef9ec9a6a60cfb93e99
Ivan02A <114321245+Ivan02A@users.noreply.github.com>
2022-09-25 08:01:23 -0500

    Cambios en la pagina de red de p

---
+++ app/src/main/java/com/example/aura/RedPsicologos.java
@@ -0,0 +1,14 @@
+package com.example.aura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RedPsicologos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red_psicologos);
+    }
+}
