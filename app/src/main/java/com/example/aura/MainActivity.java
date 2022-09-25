befcb2be6543f666e98193bbb1dd64acfc5dc867
julio_meza <juliomeza2510@outlook.com>
2022-09-24 22:45:39 -0500

    Created privacy layout

--- app/src/main/java/com/example/aura/MainActivity.java
+++ app/src/main/java/com/example/aura/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.aura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.continuar);
+        CheckBox box = (CheckBox) findViewById(R.id.box);
+
+        box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+               if(box.isChecked()){
+                   btn.setEnabled(true);
+               } else {
+                   btn.setEnabled(false);
+               }
+           }
+        }
+        );
+
+
     }
+
+
+
+
+
 }
